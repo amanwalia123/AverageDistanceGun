07fead193abc4d6479f9c5f55562b2469aa10d05
aman <amanpreetwalia278@gmail.com>
2017-12-10 15:32:35 -0500

    Added Code

---
+++ README.md
@@ -0,0 +1,12 @@
+# DistanceGun
+This is  project for implementation of  problem in Brainwave Contest for IEEE-2012 event. This is implemented on arduino using ultrasonic sensor to measure average distance of moving car.
+## Getting Started
+Download or clone this repository to your local PC.
+
+### Prerequisites
+
+You need to have Arduino connected to your system.Arduino should be connected to LCD module,Ultrasonic sensor and an LED.
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
---
+++ lm35.ino
@@ -0,0 +1,18 @@
+//declare variables
+float tempC;
+int tempPin = 0;
+
+void setup()
+{
+Serial.begin(9600); //opens serial port, sets data rate to 9600 bps
+}
+
+void loop()
+{
+tempC = analogRead(tempPin);                    //read the value from the sensor
+tempC = (5.0 * tempC * 100.0)/1024.0;
+ Serial.print("Surrounding temperature is : "); //convert the analog data to temperature
+Serial.print((byte)tempC);
+Serial.print("/n");                     //send the data to the computer
+delay(1000);                           //wait one second before sending new data
+}
